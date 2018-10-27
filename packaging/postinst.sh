a6e2cdf7f7a44ab70a7f64a5865b22a759d969e1
dante-signal31 <dante.signal31@gmail.com>
2018-10-27 03:39:27 +0200

    Initial commit. Application scaffolding.

---
+++ packaging/postinst.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Link geolocate executable from /usr/bin.
+ln -s /opt/coko/bin/coko /usr/bin/coko
---
+++ packaging/postuninst.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Remove coko link in /usr/bin.
+rm /usr/bin/coko
