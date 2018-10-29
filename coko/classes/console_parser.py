6a872d5976c888d51003a45fa42c139a9623ad83
dante-signal31 <dante.signal31@gmail.com>
2018-10-29 12:54:00 +0100

    Console parsing done.

--- coko/classes/console_parser.py
+++ coko/classes/console_parser.py
@@ -3,14 +3,6 @@ import os.path
 from typing import Dict
 
 
-def _check_folder_exists(_string: str) -> str:
-    if os.path.isdir(_string):
-        return _string
-    else:
-        raise argparse.ArgumentTypeError("{0} file does "
-                                         "not exists.".format(_string))
-
-
 def parse_arguments(args: list=None) -> Dict[str, str]:
     arg_parser = argparse.ArgumentParser(description="A Tool to overwrite directories "
                                                      "using files from a different "
@@ -20,18 +12,21 @@ def parse_arguments(args: list=None) -> Dict[str, str]:
                                                 "<https://github.com/dante-signal31/coko>")
     arg_parser.add_argument(dest="source_folder",
                             metavar="SOURCE_FOLDER",
-                            nargs=1, type=_check_folder_exists, default=None,
+                            nargs=1, default=None,
                             help="Source folder where you want copy files from.")
     arg_parser.add_argument(dest="destination_folder",
                             metavar="DESTINATION_FOLDER",
-                            nargs=1, type=_check_folder_exists, default=None,
+                            nargs=1, default=None,
                             help="Destination folder where you want copy "
                                  "files to.")
     arg_parser.add_argument("-c", "--create", dest="default_ownership",
-                            nargs=3, metavar="UID GUID PERMISSION", default=[],
+                            nargs=3, metavar="UID GUID PERMISSION", default=None,
                             help="Copy over files not present at destination "
                                  "folder yet and set for them given uid and guid " \
                                  "and permission.")
-    parsed_arguments = {item: (value[0] if value!=[] else None)
+    # Parse_args returns each parameter in a list. We must take them out.
+    parsed_arguments = {item: (value[0] if item != "default_ownership"
+                               else value)
                         for (item, value) in vars(arg_parser.parse_args(args)).items()}
+
     return parsed_arguments
--- tests/test_console_parser.py
+++ tests/test_console_parser.py
@@ -15,6 +15,8 @@ class TestConsoleParser(unittest.TestCase):
         cls._destination_folder = tempfile.mkdtemp()
 
     def test_correct_input(self):
+        """ Test a correct input of two folders are properly parsed.
+        """
         parsed_arguments = console_parser.parse_arguments(f"{self.__class__._source_folder} "
                                                           f"{self.__class__._destination_folder}".split())
         self.assertEqual(self.__class__._source_folder,
@@ -22,21 +24,13 @@ class TestConsoleParser(unittest.TestCase):
         self.assertEqual(self.__class__._destination_folder,
                          parsed_arguments["destination_folder"])
 
-    def test_incorrect_input(self):
-        self.assertRaises(argparse.ArgumentTypeError,
-                          console_parser.parse_arguments,
-                          f"{self.__class__._source_folder}X ",
-                          f"{self.__class__._destination_folder}")
-        self.assertRaises(argparse.ArgumentTypeError,
-                          console_parser.parse_arguments,
-                          f"{self.__class__._source_folder} ",
-                          f"{self.__class__._destination_folder}X")
-
     def test_create_correct_arguments(self):
+        """ Test correct optional parameters are properly parsed.
+        """
         parsed_arguments = console_parser.parse_arguments(f"{self.__class__._source_folder} "
                                                           f"{self.__class__._destination_folder} "
                                                           f"--create 10 50 775".split())
-        self.assertEqual([10, 50, 775], parsed_arguments["default_ownership"])
+        self.assertEqual(["10", "50", "775"], parsed_arguments["default_ownership"])
 
 
 if __name__ == '__main__':
