9715f6a86671eb779c4a4f79f8de8b5a95b1e289
dante-signal31 <dante.signal31@gmail.com>
2018-10-30 18:02:11 +0100

    All tests cases done, except the one related to creating new files at destination and applying them default ownerships.

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -1,6 +1,10 @@
 import unittest
+import os.path
 
 import coko.launcher as launcher
+import tests.tools as tools
+import tests.test_sync as test_sync
+
 
 class TestLauncher(unittest.TestCase):
 
@@ -10,7 +14,36 @@ class TestLauncher(unittest.TestCase):
 
         :return: None
         """
-        self.assertEqual(True, False)
+        with tools.create_test_bed(test_sync.DUMMY_SOURCE_FOLDER,
+                                   test_sync.DUMMY_DESTINATION_FOLDER) as test_bed:
+            launcher.main(["coko",
+                           test_bed.source_folder,
+                           test_bed.destination_folder])
+            # TODO: Refactoring needed. Too much code repeated with test_sync.test_copy_files_default
+            for synced_file in test_sync.DUMMY_DESTINATION_FOLDER:
+                destination_file_path = os.path.join(test_bed.destination_folder,
+                                                     synced_file.relative_filename_path)
+                try:
+                    with open(destination_file_path) as f:
+                        destination_contents = f.read()
+                except FileNotFoundError:
+                    self.fail(f"A file was not found at "
+                              f"destination folder: {destination_file_path}")
+                else:
+                    # Check contents have actually changed to the ones in source
+                    # folder.
+                    source_file_path = os.path.join(test_bed.source_folder,
+                                                    synced_file.relative_filename_path)
+                    with open(source_file_path) as f:
+                        source_contents = f.read
+                    self.assertEqual(source_contents, destination_contents)
+                    # Check ownership data has been preserved.
+                    file_stat = os.stat(destination_file_path)
+                    self.assertEqual(synced_file.ownership.uid, file_stat.st_uid)
+                    self.assertEqual(synced_file.ownership.guid, file_stat.st_gid)
+                    self.assertEqual(synced_file.ownership.permissions, file_stat.st_mode)
+
+
 
     def test_launcher_create(self)-> None:
         """ Test aplication synchcronizing a source file over a destination
@@ -19,6 +52,7 @@ class TestLauncher(unittest.TestCase):
 
         :return: None
         """
+        # TODO: Pending.
         self.assertEqual(True, False)
 
 
