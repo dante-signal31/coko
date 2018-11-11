5fc7ebb402509ab0c52c03c25bf764428e93c1db
dante-signal31 <dante.signal31@gmail.com>
2018-11-11 02:29:21 +0100

    Path to installed manpage fixed.

--- packaging/postinst.sh
+++ packaging/postinst.sh
@@ -4,4 +4,4 @@
 ln -s /opt/coko/bin/coko /usr/bin/coko
 
 # Link man page from /usr/share/man/man1
-ln -s /opt/coko/share/man/man1/coko.1 /usr/share/man/man1/coko.1
+ln -s /opt/coko/lib/python3.7/site-packages/coko-1.0.5-py3.7.egg/share/man/man1/coko.1 /usr/share/man/man1/coko.1
