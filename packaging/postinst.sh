2f9d163ff8825c133b2536ca706eb988154904f6
dante-signal31 <dante.signal31@gmail.com>
2018-11-17 01:07:15 +0100

    Now postinst script os dynamically generated to include coorect path to manpage.

--- packaging/postinst.sh
+++ packaging/postinst.sh
@@ -4,4 +4,4 @@
 ln -s /opt/coko/bin/coko /usr/bin/coko
 
 # Copy man page to /usr/share/man/man1
-cp /opt/coko/lib/python3.7.1/site-packages/coko-1.0.9-py3.7.1.egg/share/man/man1/coko.1 /usr/share/man/man1/.
+cp /opt/coko/lib/python3.7/site-packages/coko-1.0.9-py3.7.egg/share/man/man1/coko.1 /usr/share/man/man1/.
